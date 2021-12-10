ab6f63d52d0dc3232d41758fcf6cbfedd7ac936f
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-10 15:33:14 +0900

    Templete Refs in the Setup Function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="home">
-    home
-    <p>My name is {{ name }} and age is {{ age }}</p>
+    <h1>home</h1>
+    <p ref="p">My name is {{ name }} and age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "Home",
   setup() {
-    console.log(" setup");
+    const p = ref(null);
+    console.log(p, p.value);
 
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("click");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "test";
     };
 
     return {
       name,
       age,
       handleClick,
+      p,
     };
   },
 };
