51138d781463bd2c55bd14060fd3bcdc74b06e91
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-11 14:16:07 +0900

    useRouter

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -15,12 +15,17 @@
 
 <script>
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 export default {
   setup() {
     const title = ref("");
     const body = ref("");
     const tags = ref([]);
     const tag = ref("");
+    const router = useRouter();
+    //console.log(router)
+    //router.go(1)
+    //router.go(-1)
     const handleKeydown = () => {
       if (!tags.value.includes(tag.value)) {
         tag.value = tag.value.replace(/\s/g, ""); // remove all whitespace
@@ -28,22 +33,20 @@ export default {
       }
       tag.value = "";
     };
-
     const handleSubmit = async () => {
       const post = {
-        // id: Math.floor(Math.random() * 10000),
+        id: Math.floor(Math.random() * 10000),
         title: title.value,
         body: body.value,
         tags: tags.value,
       };
-
       await fetch("http://localhost:3000/posts", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(post),
       });
+      router.push({ name: "Home" });
     };
-
     return { body, title, tags, tag, handleKeydown, handleSubmit };
   },
 };
