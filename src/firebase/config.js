9df5d19e8b27517f655a8904c79627242671b5f0
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-11 16:19:15 +0900

    firebase project

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCRHtP27HKX-OjJjagcVpmZ3rlx07kR_8g",
+  authDomain: "udamy-vue-firebase-sites.firebaseapp.com",
+  projectId: "udamy-vue-firebase-sites",
+  storageBucket: "udamy-vue-firebase-sites.appspot.com",
+  messagingSenderId: "607405705991",
+  appId: "1:607405705991:web:4413f995b2eddb824893a2",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore service
+const projectFirestore = firebase.firestore();
+
+export default { projectFirestore };
