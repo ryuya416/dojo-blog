c09c71f9b503b5b1fb75d1ba12eec4754550b3a1
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-11 11:23:03 +0900

    Composables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,28 +9,15 @@
 </template>
 
 <script>
-import { ref } from "vue";
-// component imports
 import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
+
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("no available data");
-        }
-        posts.value = await data.json();
-        console.log(posts.value);
-      } catch (err) {
-        error.value = err.message;
-        console.log(error.value);
-      }
-    };
+    const { posts, error, load } = getPosts();
+
     load();
 
     return { posts, error };
