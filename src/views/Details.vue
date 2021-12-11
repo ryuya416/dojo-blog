0afb57f01274c708fefea0fce8ce3a2e983329e4
ryuya416 <67462677+ryuya416@users.noreply.github.com>
2021-12-11 22:22:07 +0900

    delete

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -3,6 +3,7 @@
   <div v-if="post" class="post">
     <h3>{{ post.title }}</h3>
     <p class="pre">{{ post.body }}</p>
+    <button @click="handleClick" class="delete">delete post</button>
   </div>
   <div v-else>
     <Spinner />
@@ -10,8 +11,9 @@
 </template>
 
 <script>
-import getPost from "@/composables/getPost";
-import { useRoute } from "vue-router";
+import getPost from "../composables/getPost";
+import { useRoute, useRouter } from "vue-router";
+import { projectFirestore } from "../firebase/config";
 // component imports
 import Spinner from "../components/Spinner.vue";
 export default {
@@ -20,16 +22,18 @@ export default {
   setup(props) {
     const route = useRoute();
     const { error, post, load } = getPost(route.params.id);
+    const router = useRouter();
     load();
-    return { error, post };
+    const handleClick = async () => {
+      await projectFirestore.collection("posts").doc(props.id).delete();
+      router.push("/");
+    };
+    return { error, post, handleClick };
   },
 };
 </script>
 
 <style scoped>
-.tags a {
-  margin-right: 10px;
-}
 .post {
   max-width: 1200px;
   margin: 0 auto;
@@ -41,5 +45,8 @@ export default {
 }
 .pre {
   white-space: pre-wrap;
+}
+button.delete {
+  margin: 10px auto;
 }
 </style>
